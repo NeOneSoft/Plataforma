f542f25a483fcae699907764080cafe5a8a2e163
Gonzalo Romero <gonclapton@hotmail.com>
2020-07-22 16:41:47 -0500

    CREATE product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,22 @@
+# Django
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+# Validator Function
+def validate_discount_value(value, discount_value):
+    if discount_value > value:
+        return value
+    else:
+        raise ValidationError("Invalid discount value")
+
+
+class Product(models.Model):
+    name = models.CharField(min_lenght=3, max_length=55)
+    value = models.FloatField(validators=[MaxValueValidator(99998.9), MinValueValidator(1)])
+    discount_value = models.FloatField(validators=[validate_discount_value])
+    stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return self.name
